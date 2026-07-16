87e61112a20ec68f7402428aeb0975f0010d6352
NirFlaze <bogdanksenofontov2005@gmail.com>
2026-07-17 00:48:36 +0300

    Corrected models

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,19 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
+
+  validates :follower_id,
+            uniqueness: {
+              scope: :following_id
+            }
+
+  validate :cannot_follow_yourself
+
+  private
+
+  def cannot_follow_yourself
+    if follower_id == following_id
+      errors.add(:following_id, "Нельзя подписаться на самого себя")
+    end
+  end
 end
