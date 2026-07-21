5627dc3f5ae57b3fb958aebfe74eb9bfe8b9bdf4
NirFlaze <bogdanksenofontov2005@gmail.com>
2026-07-21 23:42:33 +0300

    Fix test for search

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:one)
+
     get search_index_url
     assert_response :success
   end
